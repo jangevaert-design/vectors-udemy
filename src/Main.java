6e8c09d1b6598be798c443cc9b8df92de6094588
Jan Gevaert <jan.gevaert@gmail.com>
2020-12-31 11:17:14 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Vector;
+
+// difference with ArrayList is that Vector is synchronized hence thread safe.
+// keep in
+public class Main {
+
+    public static void main(String[] args) {
+
+      List<Employee> employeeList = new Vector<>();
+      employeeList.add(new Employee("Jane", "Jones", 123));
+      employeeList.add(new Employee("John", "Doe", 4567));
+      employeeList.add(new Employee("Mary", "Smith", 22));
+      employeeList.add(new Employee("Mike", "Wilson", 3245));
+    }
+
+}
